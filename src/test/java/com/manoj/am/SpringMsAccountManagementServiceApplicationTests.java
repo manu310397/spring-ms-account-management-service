0a8c30db2f9e59e78ef1b4e5317534150cc9908e
manu310397 <manojb912@gmail.com>
2022-11-18 09:53:48 +0530

    Added initial configuration

---
+++ src/test/java/com/manoj/am/SpringMsAccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.am;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsAccountManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
